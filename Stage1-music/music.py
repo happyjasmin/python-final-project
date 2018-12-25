fb23943e3208c6a2d183399a2931bf3f1b9185bc
redjason <happyjasmine1314je@gmail.com>
2018-12-25 23:04:14 +0800

    添加音樂與撥放鍵
    1.  如果滑鼠按play會撥放校歌，按鍵轉為stop
    2.  如果按stop會停止撥放，按鍵轉回play

--- Stage1-music/music.py
+++ Stage1-music/music.py
@@ -11,20 +11,36 @@ screen = pygame.display.set_mode((width,height))
 	
 pygame.mixer.init()
 
+
+# 物件圖片
+blue  	= pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\blue.png")
+red   	= pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\orange.png")
+green 	= pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\green.png")
+play	= pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\play.png")
+stop 	= pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\stop.png")
+
+
+#加聲音
+hit  = pygame.mixer.Sound("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\Air_Woosh_Underwater.wav")
+song = pygame.mixer.music.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\ntu.wav")
+
+hit.set_volume(1)
+
+
 # WASD按鍵狀況記錄
 keys = [False,False,False,False]
 
-# 物件圖片
-blue = pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\blue.png")
-red = pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\orange.png")
-green = pygame.image.load("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\green.png")
+#是否點擊play/stop
+clickbutton = False
 
-#加聲音
-hit = pygame.mixer.Sound("C:\\Users\\user\\Documents\\GitHub\\test\\Stage1-music\\Air_Woosh_Underwater.wav")
-hit.set_volume(0.05)
+#是否在撥放音樂 
+playing = False
 
 
-button = blue
+#使用者未按任何按鍵的預設狀態
+button 	= blue
+music 	= play
+
 
 while 1 :
 	
@@ -32,8 +48,8 @@ while 1 :
 	
 	screen.blit(button,(0,100))
 	screen.blit(button,(300,100))
+	screen.blit(music, (500,500))
 	
-		
 	pygame.display.flip()
 	time.sleep(0.2)
 	
@@ -67,12 +83,35 @@ while 1 :
 				keys[2] = False
 			elif event.key == pygame.K_d :
 				keys[3] = False
+
 				
-	if keys[0] :
+		#偵測是否有按滑鼠，與是否有按在"play"上		
+		if event.type == pygame.MOUSEBUTTONDOWN and 500 <= event.pos[0] <= (500+351) and 500 <= event.pos[1] <= (500+153) :
+			
+			clickbutton = True
+			playing = not(playing)
+			
 		
+	#如果按下w			
+	if keys[0] :		
+	
 		hit.play()
 		button=red
+		
 	else :
 		button = blue
+		
+	#如果點play/stop
+	if clickbutton and playing:	
+	
+		pygame.mixer.music.play()
+		music = stop
+		clickbutton = False 	
+	
+	elif clickbutton and not(playing) :
+		
+		pygame.mixer.music.stop()		
+		music = play
+		clickbutton = False
 		
 		
