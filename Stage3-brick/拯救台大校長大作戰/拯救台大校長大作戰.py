c2db5b0ecf664de6ab06a76f325e7eb110084865
SkySpiriT <b04602031.ntu.edu.tw>
2019-01-06 13:10:36 +0800

    reset

--- Stage3-brick/拯救台大校長大作戰/拯救台大校長大作戰.py
+++ Stage3-brick/拯救台大校長大作戰/拯救台大校長大作戰.py
@@ -4,9 +4,13 @@ import time
 import random  
 import os
 import math
+<<<<<<< HEAD
+os.chdir('C:\\Users\\user\\Documents\\GitHub\\test\\拯救台大校長大作戰') #package資料夾所在目錄
+=======
 os.chdir('D:/PBC') #package資料夾所在目錄
 from package.OP_ED import Opening_Trailer1,Opening_Trailer2,Opening_Trailer3,Failure_screen
 from package.game_loop import stage1,stage2,stage3
+>>>>>>> 5939d5098b9a1b34b2def0567ee7ce744a62b2de
 
 
 
